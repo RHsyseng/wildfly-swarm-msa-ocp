b5c7fd132476c2a566d12ac48ed2fd5976b6bf2e
Babak Mozaffari <bmozaffa@redhat.com>
2017-10-10 16:06:50 -0700

    Reverse proxy draft implementation refactored and implemented for other HTTP methods... details remain, including other connection details and use of request body for methods other than POST and PUT

--- Edge/src/main/java/com/redhat/refarch/obsidian/brownfield/lambdaair/service/EdgeService.java
+++ Edge/src/main/java/com/redhat/refarch/obsidian/brownfield/lambdaair/service/EdgeService.java
@@ -33,7 +33,48 @@ public class EdgeService extends HttpServlet
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
-		logger.fine( "Caller IP address is " + GlobalTracer.get().activeSpan().getBaggageItem( "forwarded-for" ) ); //TODO inject tracer?
+		handle( request, response, "GET", false );
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		handle( request, response, "POST", true );
+	}
+
+	@Override
+	protected void doHead(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		handle( request, response, "HEAD", false );
+	}
+
+	@Override
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		handle( request, response, "PUT", true );
+	}
+
+	@Override
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		handle( request, response, "DELETE", false );
+	}
+
+	@Override
+	protected void doOptions(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		handle( request, response, "OPTIONS", false );
+	}
+
+	@Override
+	protected void doTrace(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		handle( request, response, "TRACE", false );
+	}
+
+	private void handle(HttpServletRequest request, HttpServletResponse response, String method, boolean hasBody) throws ServletException, IOException
+	{
+		logger.fine( "Caller IP address is " + request.getHeader( "uberctx-forwarded-for" ) );
 		String fullPath = request.getPathInfo();
 		logger.fine( "Path " + fullPath );
 		String[] segments = getPathSegments( fullPath );
@@ -59,7 +100,16 @@ public class EdgeService extends HttpServlet
 				connection.addRequestProperty( key, value );
 			}
 		}
-		connection.setRequestMethod( "GET" );
+		connection.setRequestMethod( method );
+		connection.setDoOutput( hasBody );
+		if( hasBody )
+		{
+			BufferedReader body = request.getReader();
+			try( PrintWriter bodyWriter = new PrintWriter( new OutputStreamWriter( connection.getOutputStream() ) ) )
+			{
+				body.lines().forEach( bodyWriter::println );
+			}
+		}
 		int responseCode = connection.getResponseCode();
 		logger.info( "Received " + responseCode + " from " + url + " while forwarding" );
 		for( Map.Entry<String, List<String>> headerEntry : connection.getHeaderFields().entrySet() )
@@ -111,75 +161,6 @@ public class EdgeService extends HttpServlet
 		return segments;
 	}
 
-	@Override
-	protected void doHead(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-	{
-		super.doHead( request, response );
-	}
-
-	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-	{
-		logger.fine( "Caller IP address is " + request.getHeader( "uberctx-forwarded-for" ) );
-		String fullPath = request.getPathInfo();
-		logger.fine( "Path " + fullPath );
-		String[] segments = getPathSegments( fullPath );
-		logger.fine( "segments [" + segments[0] + "] and [" + segments[1] + "]" );
-		String query = request.getQueryString();
-		logger.fine( "Query " + query );
-
-		String url = getHost( segments[0] ) + segments[1];
-		if( query != null )
-		{
-			url += "?" + query;
-		}
-		HttpURLConnection connection = (HttpURLConnection)new URL( url ).openConnection();
-		Enumeration<String> headerKeys = request.getHeaderNames();
-		while( headerKeys.hasMoreElements() )
-		{
-			String key = headerKeys.nextElement();
-			Enumeration<String> values = request.getHeaders( key );
-			while( values.hasMoreElements() )
-			{
-				String value = values.nextElement();
-				logger.fine( "Request header " + key + ": " + value );
-				connection.addRequestProperty( key, value );
-			}
-		}
-		connection.setRequestMethod( "POST" );
-		connection.setDoOutput( true );
-		connection.getOutputStream();
-		BufferedReader body = request.getReader();
-		try( PrintWriter bodyWriter = new PrintWriter( new OutputStreamWriter( connection.getOutputStream() ) ) )
-		{
-			body.lines().forEach( bodyWriter::println );
-		}
-		int responseCode = connection.getResponseCode();
-		logger.info( "Received " + responseCode + " from " + url + " while forwarding" );
-		for( Map.Entry<String, List<String>> headerEntry : connection.getHeaderFields().entrySet() )
-		{
-			if( headerEntry.getKey() == null )
-			{
-				logger.fine( "Response header null: " + headerEntry.getValue() );
-				continue;
-			}
-			for( String value : headerEntry.getValue() )
-			{
-				logger.fine( "Response header " + headerEntry.getKey() + ": " + value );
-				response.addHeader( headerEntry.getKey(), value );
-			}
-		}
-		if( responseCode < 300 )
-		{
-			BufferedReader bufferedReader = new BufferedReader( new InputStreamReader( connection.getInputStream() ) );
-			bufferedReader.lines().forEach( response.getWriter()::println );
-		}
-		else
-		{
-			response.sendError( responseCode, connection.getResponseMessage() );
-		}
-	}
-
 	private String getHost(String context)
 	{
 		String host = mapping.getHost( context );
@@ -201,29 +182,5 @@ public class EdgeService extends HttpServlet
 			}
 		}
 		return host;
-	}
-
-	@Override
-	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-	{
-		super.doPut( request, response );
-	}
-
-	@Override
-	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-	{
-		super.doDelete( request, response );
-	}
-
-	@Override
-	protected void doOptions(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-	{
-		super.doOptions( request, response );
-	}
-
-	@Override
-	protected void doTrace(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-	{
-		super.doTrace( request, response );
 	}
 }
